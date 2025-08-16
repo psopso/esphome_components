e86fc24149706b9a605ed4e8691bc4752ed431e8
psopso <57403373+psopso@users.noreply.github.com>
2025-08-16 13:42:00 +0200

    Update xt211_dlms.h

--- components/xt211_dlms/xt211_dlms.h
+++ components/xt211_dlms/xt211_dlms.h
@@ -1,21 +1,44 @@
 #pragma once
-#include "esphome.h"
-#include "esphome/components/uart/uart.h"
 
+#include "esphome/core/component.h"           // PollingComponent
+#include "esphome/components/uart/uart.h"     // uart::UARTDevice
+#include "esphome/components/sensor/sensor.h" // sensor::Sensor
+
+namespace esphome {
 namespace xt211_dlms {
 
-class Xt211DlmsComponent : public PollingComponent, public esphome::uart::UARTDevice {
+class Xt211DlmsComponent : public PollingComponent, public uart::UARTDevice {
  public:
-  Xt211DlmsComponent(uart::UARTComponent *parent, int dir_pin) : UARTDevice(parent), dir_pin_(dir_pin) {}
+  // Konstruktor: parent = UARTComponent (z YAML), dir_pin = RS485 DE/RE pin (nebo -1 pokud nepoužíváš)
+  Xt211DlmsComponent(uart::UARTComponent *parent, int dir_pin);
+
+  // Volitelné přiřazení senzorů (mohou zůstat nullptr, pak se nepoužijí)
+  void set_voltage_sensor(sensor::Sensor *s) { this->voltage_sensor_ = s; }
+  void set_current_sensor(sensor::Sensor *s) { this->current_sensor_ = s; }
+
+  // ESPHome lifecycle
   void setup() override;
-  void update() override;
+  void loop() override;    // čteme průběžně z UARTu
+  void update() override;  // periodické úlohy (ponecháno prázdné)
 
  protected:
-  int dir_pin_;
-  float voltage_{0};
-  float current_{0};
+  // HW
+  int dir_pin_{-1};
 
-  void parse_frame_();
+  // RX buffer / stav
+  static constexpr int MAX_FRAME = 512;
+  uint8_t buf_[MAX_FRAME];
+  int pos_{0};
+  bool in_frame_{false};
+
+  // Volitelné senzory
+  sensor::Sensor *voltage_sensor_{nullptr};
+  sensor::Sensor *current_sensor_{nullptr};
+
+  // Interní metody
+  void handle_byte_(uint8_t b);
+  void process_frame_(const uint8_t *frame, int len);
 };
 
 }  // namespace xt211_dlms
+}  // namespace esphome
