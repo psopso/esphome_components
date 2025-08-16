6477ef5b2dfd33bb1782fb9cdc6e99be2e239028
psopso <57403373+psopso@users.noreply.github.com>
2025-08-16 21:00:55 +0200

    Update xt211_dlms.h

--- components/xt211_dlms/sensor/xt211_dlms.h
+++ components/xt211_dlms/sensor/xt211_dlms.h
@@ -10,7 +10,7 @@ class Xt211Dlms : public PollingComponent {
  public:
   void set_uart(uart::UARTComponent *uart) { this->uart_ = uart; }
   void set_dir_pin(int pin) { this->dir_pin_ = pin; }
-  void add_sensor(Sensor *sensor, const std::string &obis) {
+  void add_sensor(sensor::Sensor *sensor, const std::string &obis) {
     sensors_.push_back(sensor);
     obis_codes_.push_back(obis);
   }
