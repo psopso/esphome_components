181498e83c1a49ddaa41fe44155314ad18f5440b
psopso <57403373+psopso@users.noreply.github.com>
2025-08-16 17:21:04 +0200

    Update sensor.py

---
+++ components/xt211_dlms/sensor.py
@@ -0,0 +1,63 @@
+# In sensor.py
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.components import sensor, uart
+from esphome.const import (
+    CONF_ID,
+    CONF_NAME,
+    CONF_OBIS_CODE,
+    # ... import other constants you need like UNIT_WATT, ICON_FLASH etc.
+)
+
+# Závislost na UART komponentě
+DEPENDENCIES = ["uart"]
+
+# Deklarace C++ jmenného prostoru
+xt211_dlms_ns = cg.esphome_ns.namespace("xt211_dlms")
+
+# Hlavní třída komponenty (hub)
+XT211DLMSHub = xt211_dlms_ns.class_("XT211DLMSHub", cg.PollingComponent, uart.UARTDevice)
+# Třída pro jednotlivý senzor
+XT211DLMSSensor = xt211_dlms_ns.class_("XT211DLMSSensor", sensor.Sensor, cg.Component)
+
+# Schéma pro konfiguraci senzoru v YAML
+SENSOR_SCHEMA = sensor.sensor_schema(
+    unit_of_measurement=...,
+    icon=...,
+    accuracy_decimals=...,
+).extend(
+    {
+        # Odkaz na hub, ke kterému tento senzor patří
+        cv.Required("xt211_dlms_id"): cv.use_id(XT211DLMSHub),
+        # OBIS kód, který má tento senzor číst
+        cv.Required(CONF_OBIS_CODE): cv.string,
+    }
+)
+
+# Celkové schéma
+CONFIG_SCHEMA = cv.All(
+    cv.Schema(
+        {
+            # Umožní vytvořit více senzorů z této platformy
+            cv.GenerateID(): cv.declare_id(XT211DLMSSensor),
+        }
+    ).extend(SENSOR_SCHEMA),
+)
+
+
+# Funkce, která převede YAML konfiguraci na C++ kód
+async def to_code(config):
+    # Najde hub podle zadaného ID
+    hub = await cg.get_variable(config["xt211_dlms_id"])
+    # Vytvoří novou instanci senzoru
+    var = cg.new_Pvariable(config[CONF_ID])
+    
+    # Inicializace senzoru
+    await cg.register_component(var, config)
+    await sensor.register_sensor(var, config)
+
+    # Nastavení OBIS kódu
+    cg.add(var.set_obis_code(config[CONF_OBIS_CODE]))
+    
+    # Zaregistrování senzoru u hubu - TOTO JE KLÍČOVÉ!
+    cg.add(hub.register_sensor(var))
