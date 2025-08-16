c35cfb6abaa1f1144303020d7d49cbcc16f9ac2b
psopso <57403373+psopso@users.noreply.github.com>
2025-08-16 12:53:04 +0200

    Update __init__.py

---
+++ components/xt211_dlms/__init__.py
@@ -0,0 +1,20 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.components import uart, sensor
+from esphome.const import CONF_ID, CONF_PIN
+
+DEPENDENCIES = ["uart"]
+
+xt211_dlms_ns = cg.esphome_ns.namespace("xt211_dlms")
+Xt211DlmsComponent = xt211_dlms_ns.class_("Xt211DlmsComponent", cg.PollingComponent, uart.UARTDevice)
+
+CONFIG_SCHEMA = cv.Schema({
+    cv.GenerateID(): cv.declare_id(Xt211DlmsComponent),
+    cv.Required("uart_id"): cv.use_id(uart.UARTComponent),
+    cv.Required("dir_pin"): cv.gpio_pin,
+}).extend(cv.polling_component_schema("1000ms"))
+
+def to_code(config):
+    var = cg.new_Pvariable(config[cv.GenerateID()], config["uart_id"], config["dir_pin"])
+    cg.add(var)
+    cg.register_component(var)
