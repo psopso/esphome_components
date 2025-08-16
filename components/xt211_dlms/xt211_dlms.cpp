0cc5e8388eadca242ebca5d405ba2b1413f6eceb
psopso <57403373+psopso@users.noreply.github.com>
2025-08-16 12:40:33 +0200

    Delete files

--- components/xt211_dlms/xt211_dlms.cpp
+++
@@ -1,8 +0,0 @@
-#include "xt211_dlms.h"
-#include "esphome/core/log.h"
-
-namespace esphome {
-namespace xt211_dlms {
-// parser je kompletně v hlavičce
-}  // namespace xt211_dlms
-}  // namespace esphome
