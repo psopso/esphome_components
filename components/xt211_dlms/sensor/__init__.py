c049d9405a012bcc559181fb88be26730afa32ed
psopso <57403373+psopso@users.noreply.github.com>
2025-08-16 20:30:11 +0200

    Update __init__.py

--- components/xt211_dlms/sensor/__init__.py
+++ components/xt211_dlms/sensor/__init__.py
@@ -1,6 +1,6 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.const import CONF_ID, CONF_OBIS_CODE, CONF_UPDATE_INTERVAL, UNIT_EMPTY, ICON_FLASH
+from esphome.const import CONF_ID, CONF_UPDATE_INTERVAL, UNIT_EMPTY, ICON_FLASH
 from esphome.components import sensor, uart
 
 DEPENDENCIES = ["uart"]
