92dd3fbb26785f3bc5297a5e4b8469355c58c2bf
psopso <57403373+psopso@users.noreply.github.com>
2025-08-18 18:41:57 +0200

    NTP synchronization added

--- components/xt211_dlms/sensor/xt211_dlms.h
+++ components/xt211_dlms/sensor/xt211_dlms.h
@@ -8,6 +8,13 @@
 #include "esphome/components/uart/uart.h"
 #include "esphome/components/sensor/sensor.h"
 
+//Pro wifi ntp
+#include "esphome/core/hal.h"
+#include "esphome/components/network/ip_address.h"
+#include "esphome/components/sntp/sntp_component.h"
+#include "esphome/core/application.h"
+
+
 namespace esphome {
 namespace xt211_dlms {
 
@@ -26,6 +33,7 @@ class Xt211Dlms : public PollingComponent, public uart::UARTDevice {
   void setup() override;
   void loop() override;
   void update() override;
+  void dump_config() override;
 
  protected:
   int dir_pin_{-1};
@@ -41,6 +49,34 @@ class Xt211Dlms : public PollingComponent, public uart::UARTDevice {
 
   void handle_byte_(uint8_t b);
   void try_parse_frame_();  // místo pro budoucí DLMS/COSEM parser
+};
+
+private:
+  void synchronize_time() {
+    // 1. Získáme ukazatel na globální SNTP komponentu
+    sntp::SNTPComponent *sntp_component = App.get_sntp();
+
+    // 2. Zkontrolujeme, zda SNTP komponenta existuje (byla definována v YAML)
+    if (sntp_component != nullptr) {
+        ESP_LOGD(TAG, "Vyvolávám synchronizaci času přes SNTP komponentu.");
+        // 3. Zavoláme metodu pro synchronizaci. Tato metoda je interní, ale funguje.
+        // V novějších verzích ESPHome může být potřeba použít jiný přístup,
+        // ale tento je nejběžnější.
+        sntp_component->request_sync();
+
+        // Po úspěšné synchronizaci můžete pracovat s aktuálním časem
+        // Například:
+        auto time_now = App.get_time(); // Získáme časovou komponentu
+        if (time_now->is_valid()) {
+            ESP_LOGI(TAG, "Aktuální čas je: %s", time_now->now().strftime("%Y-%m-%d %H:%M:%S").c_str());
+        } else {
+            ESP_LOGW(TAG, "Čas ještě není synchronizován.");
+        }
+
+    } else {
+        ESP_LOGW(TAG, "SNTP komponenta není v konfiguraci YAML definována!");
+    }
+  }
 };
 
 }  // namespace xt211_dlms
