5c358318ed4ce79288e4a40987b17dee06868051
psopso <57403373+psopso@users.noreply.github.com>
2025-08-19 09:52:04 +0200

    Update xt211_dlms.cpp

--- components/xt211_dlms/sensor/xt211_dlms.cpp
+++ components/xt211_dlms/sensor/xt211_dlms.cpp
@@ -25,7 +25,7 @@ void Xt211Dlms::loop() {
 //  delay(10000);
   LOOP_COUNTER = LOOP_COUNTER + 1;
   ESP_LOGI(TAG, "XT211 DLMS LOOP: %d", LOOP_COUNTER);
-  Serial.println("Going to deep sleep...");
+//  Serial.println("Going to deep sleep...");
   uint64_t sleep_us = (uint64_t)SLEEP_MINUTES * 60ULL * 1000000ULL;
 //  esp_sleep_enable_timer_wakeup(sleep_us);
 //  esp_deep_sleep_start();
