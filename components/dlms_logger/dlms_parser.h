99b8854cab3aa6f1e77f3ae728100bb185131952
psopso <57403373+psopso@users.noreply.github.com>
2025-08-29 19:11:00 +0200

    Update dlms_parser.h

--- components/dlms_logger/dlms_parser.h
+++ components/dlms_logger/dlms_parser.h
@@ -2,10 +2,11 @@
 
 #include <string>
 #include <vector>
+#include <map>
 
 struct DlmsRecord {
     std::string obis;
     std::string value;   // všechno jako text (ANSI)
 };
 
-std::vector<DlmsRecord> parseDlmsResponse(const uint8_t* buf, size_t len);
+std::map<std::string, std::string> parse_dlms(const uint8_t *buf, size_t len);
