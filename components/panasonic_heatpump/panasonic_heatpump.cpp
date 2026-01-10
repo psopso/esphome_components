7082f0b8ceb667b02692620f475d5b45fea7fd4c
psopso <57403373+psopso@users.noreply.github.com>
2026-01-10 20:20:41 +0100

    Update panasonic_heatpump.cpp

--- components/panasonic_heatpump/panasonic_heatpump.cpp
+++ components/panasonic_heatpump/panasonic_heatpump.cpp
@@ -30,7 +30,7 @@ void PanasonicHeatpumpComponent::update() {
 void PanasonicHeatpumpComponent::loop() {
   //My timer application
   if ((esp_timer_get_time() / 1000 - timer1) > 30000){
-    ESP_LOGW(TAG, "My test timer - %u  %u", this->uart_client_timeout_, this->last_request_time_);
+    ESP_LOGW(TAG, "My test timer - %d  %u  %u  %u", this->uart_client_ != nullptr, this->uart_client_timeout_, this->last_request_time_, esp_timer_get_time() / 1000);
     timer1 = esp_timer_get_time() / 1000;
   }
   
